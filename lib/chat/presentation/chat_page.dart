ada032521d4731a80f4a24a739b4a64b3777e226
0xAhmd <ahmedhesham200987@gmail.com>
2025-04-29 18:50:50 +0300

    app icon changed

--- lib/chat/presentation/chat_page.dart
+++ lib/chat/presentation/chat_page.dart
@@ -34,111 +34,114 @@ class _ChatPageState extends State<ChatPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Consumer<ChatProvider>(
+    return SafeArea(
+      
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Consumer<ChatProvider>(
+                builder: (context, chatProvider, child) {
+                  if (chatProvider.messages.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'Start a convo..',
+                        style: TextStyle(fontSize: 22, color: Colors.white),
+                      ),
+                    );
+                  }
+                  return ListView.builder(
+                    controller: _scrollController,
+                    itemBuilder: (context, index) {
+                      final message = chatProvider.messages[index];
+                      return ChatBubble(message: message);
+                    },
+                    itemCount: chatProvider.messages.length,
+                  );
+                },
+              ),
+            ),
+      
+            Consumer<ChatProvider>(
               builder: (context, chatProvider, child) {
-                if (chatProvider.messages.isEmpty) {
-                  return const Center(
-                    child: Text(
-                      'Start a convo..',
-                      style: TextStyle(fontSize: 22, color: Colors.white),
-                    ),
+                if (chatProvider.isLoading) {
+                  return  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Center(child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 180.0),
+                      child: LinearProgressIndicator(
+                        color: Colors.white,
+                        backgroundColor: Colors.grey[600],
+                      ))),
                   );
                 }
-                return ListView.builder(
-                  controller: _scrollController,
-                  itemBuilder: (context, index) {
-                    final message = chatProvider.messages[index];
-                    return ChatBubble(message: message);
-                  },
-                  itemCount: chatProvider.messages.length,
-                );
+                return const SizedBox.shrink();
               },
             ),
-          ),
-
-          Consumer<ChatProvider>(
-            builder: (context, chatProvider, child) {
-              if (chatProvider.isLoading) {
-                return  Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: Center(child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 180.0),
-                    child: LinearProgressIndicator(
-                      color: Colors.white,
-                      backgroundColor: Colors.grey[600],
-                    ))),
-                );
-              }
-              return const SizedBox.shrink();
-            },
-          ),
-
-
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16.0,
-                    vertical: 8.0,
-                  ),
-                  child: TextField(
-                    cursorColor: Colors.white,
-                    style: const TextStyle(color: Colors.white),
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintStyle: const TextStyle(color: Colors.white),
-                      hintText: 'Type a message',
-                      contentPadding: const EdgeInsets.symmetric(
-                        vertical: 15.0,
-                        horizontal: 16.0,
-                      ),
-
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey[300]!,
-                          width: 2.0,
+      
+      
+            Row(
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: TextField(
+                      cursorColor: Colors.white,
+                      style: const TextStyle(color: Colors.white),
+                      controller: _controller,
+                      decoration: InputDecoration(
+                        hintStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Type a message',
+                        contentPadding: const EdgeInsets.symmetric(
+                          vertical: 15.0,
+                          horizontal: 16.0,
                         ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.grey[300]!,
-                          width: 2.0,
+      
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[300]!,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        borderRadius: BorderRadius.circular(8.0),
+      
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey[300]!,
+                            width: 2.0,
+                          ),
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(right: 16.0),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.black,
+                Container(
+                  margin: const EdgeInsets.only(right: 16.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.send, color: Colors.white),
+                    onPressed: () {
+                      final content = _controller.text.trim();
+                      if (content.isNotEmpty) {
+                        context.read<ChatProvider>().sendMessage(content);
+                        _controller.clear();
+                        FocusScope.of(context).unfocus();
+                        _scrollToBottom(); 
+                      }
+                    },
+                  ),
                 ),
-                child: IconButton(
-                  icon: const Icon(Icons.send, color: Colors.white),
-                  onPressed: () {
-                    final content = _controller.text.trim();
-                    if (content.isNotEmpty) {
-                      context.read<ChatProvider>().sendMessage(content);
-                      _controller.clear();
-                      FocusScope.of(context).unfocus();
-                      _scrollToBottom(); 
-                    }
-                  },
-                ),
-              ),
-            ],
-          ),
-        ],
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
